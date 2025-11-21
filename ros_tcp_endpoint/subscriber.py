711298ad0f6b45d433422d252bbb20b412fc1375
eking158 <158@.>
2025-11-21 18:34:51 +0900

    [improve] fork받아서 기존에 자체적으로 수정한 부분 반영

--- ros_tcp_endpoint/subscriber.py
+++ ros_tcp_endpoint/subscriber.py
@@ -43,7 +43,10 @@ class RosSubscriber(RosReceiver):
         self.tcp_server = tcp_server
         self.queue_size = queue_size
 
-        qos_profile = QoSProfile(depth=queue_size)
+        qos_profile = QoSProfile(depth=queue_size,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            durability=QoSDurabilityPolicy.VOLATILE
+            )
 
         # Start Subscriber listener function
         self.subscription = self.create_subscription(
